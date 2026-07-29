a5063f5e54d596e21c64732dd904c370268a1f58
ayushrana124 <ayushrana73107@gmail.com>
2026-07-29 23:46:13 +0530

    create util function for github scrapping

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,35 +2,33 @@ import express from "express";
 import { PORT } from "./src/config/config";
 import { PreInterviewBody } from "./src/types/types";
 import axios from "axios";
+import { scrapeGithub } from "./src/scraper/github";
 
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 
 app.post("/api/v1/pre-interview", async (req, res) => {
     const { success, data } = PreInterviewBody.safeParse(req.body);
-    
+    console.log(success, data);
     if(!success){
         return res.status(400).json({ error: "Incorrect body" });
     }
-
+    
     //Url can be malformed, use SLM here
     const githubUrl = data.github.endsWith("/") ? data.github.slice(0, -1) : data.github;
 
 
-    const githubUsername = githubUrl.split("/").pop();
+    const githubUsername : string = githubUrl.split("/").pop() as string;
 
-    const userRepos = await axios.get(`https://api.github.com/users/${githubUsername}/repos`);
-    const filterUserRepos = userRepos.data.map((x:any) => {
-        desc: x.description,
-        name: x.name,
-        fullName: x.full_name,
-        starCount: x.stargazers_count,
-    });
-
-    console.log(filterUserRepos); 
-
-
+    const filteredRepos = await scrapeGithub(githubUsername);
+    console.log(filteredRepos);
+    
+    res.send({ repos: filteredRepos });
 
 })
 
---
+++ apps/backend/src/scraper/github.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export async function scrapeGithub(username : string ){
+    const userRepos = await axios.get(`https://api.github.com/users/${username}/repos`
+   )
+    
+   return userRepos.data.map((repo : any) => ({
+           description: repo.description,
+              name: repo.name,
+              full_name: repo.full_name,
+              starCount: repo.stargazers_count,
+   }));
+};
+
+
+
+
